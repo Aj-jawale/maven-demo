e6fc9f12bdd910dc7fbda687f5a9773a8d989d0c
ayush.github.ioommit <ayush.patil@gmail.com>
2022-07-13 22:47:49 +0530

    maven for jenkins to

---
+++ src/main/java/org/maven/demoMaven.java
@@ -0,0 +1,11 @@
+package org.maven;
+
+public class demoMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello from maven");
+
+	}
+
+}
