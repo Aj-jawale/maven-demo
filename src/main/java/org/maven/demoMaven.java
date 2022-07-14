3ec147fd2d1d95c7bd754bc3bf770f1e89d65177
Aj-jawale <109055460+Aj-jawale@users.noreply.github.com>
2022-07-14 09:54:13 +0530

    some changes in java

--- src/main/java/org/maven/demoMaven.java
+++ src/main/java/org/maven/demoMaven.java
@@ -5,6 +5,7 @@ public class demoMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello from maven");
+		System.out.println("Hello from jenkins");
 
 	}
 
